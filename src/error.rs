51fb42fdfa33d9ac713471d912c7a79204e7961f
Stephen <webmaster@scd31.com>
2023-03-13 16:45:57 -0300

    disconnect from APRS server if connection hangs

--- src/error.rs
+++ src/error.rs
@@ -1,11 +1,15 @@
 use aprs_parser::EncodeError;
 use std::{io, string};
 use thiserror::Error;
+use tokio::time::error::Elapsed;
 
 #[derive(Error, Debug)]
 pub enum ISReadError {
     #[error(transparent)]
     IoError(#[from] io::Error),
+
+    #[error("TCP read timeout - connection died")]
+    TcpReadTimeout(#[from] Elapsed),
 
     #[error("Server command was not valid UTF8")]
     ServerCommandInvalidUtf8(#[from] string::FromUtf8Error),
